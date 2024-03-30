01dbc1336ac3164102874c07dd944401da77e041
Tegibar1983 <tbtf1991@gmail.com>
2024-03-30 07:51:05 -0700

    Deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,8 +2,14 @@
 # Bash scrip to set up web servers to deploy web_static
 sudo apt-get update
 sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/releases/test/index.htm
 sudo echo "<html>
   <head>
   </head>
@@ -11,7 +17,11 @@ sudo echo "<html>
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
+
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
 sudo chown -R ubuntu:ubuntu /data
+
 sudo sed -i '53i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+
 sudo service nginx restart
