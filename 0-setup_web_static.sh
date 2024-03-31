8ee08b169efed342b0330c02d3d056f56521b851
Tegibar1983 <tbtf1991@gmail.com>
2024-03-30 20:32:34 -0700

    deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Bash scrip to set up web servers to deploy web_static
+# script that sets up web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
@@ -9,7 +9,7 @@ sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/releases/test/index.htm
+sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
   <head>
   </head>
@@ -18,10 +18,10 @@ sudo echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '53i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
